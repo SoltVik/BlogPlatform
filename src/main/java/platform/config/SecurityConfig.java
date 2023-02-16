09a4a52e3ef0fe116e1c2c5a6a534f6cae3590ae
Soltvik <106930781+SoltVik@users.noreply.github.com>
2023-02-16 11:43:36 +0100

    Added basic authentication in swagger

--- src/main/java/platform/config/SecurityConfig.java
+++ src/main/java/platform/config/SecurityConfig.java
@@ -2,19 +2,46 @@ package platform.config;
 
 import javax.sql.DataSource;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "Blog Platform API", version = "1.0", description = "User & Message Details"))
 public class SecurityConfig {
+    private static final String SCHEME_NAME = "basicAuth";
+    private static final String SCHEME = "basic";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(SCHEME_NAME, createSecurityScheme()))
+                .addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME));
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme()
+                .name(SCHEME_NAME)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme(SCHEME);
+    }
 
     @Bean
     public PasswordEncoder encoder() {
@@ -29,7 +56,7 @@ public class SecurityConfig {
                 .antMatchers("/posts", "/post/**").fullyAuthenticated()
                 .antMatchers("/reg").anonymous()
                 .antMatchers("/css/**", "/js/**", "/img/**", "/webjars/**", "/favicon.ico").permitAll()
-                .antMatchers("/v3/api-docs", "/swagger-ui/**").permitAll()
+                .antMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                     .formLogin()
@@ -38,7 +65,12 @@ public class SecurityConfig {
                 .and()
                     .logout()
                     .logoutSuccessUrl("/")
-                    .permitAll();
+                    .deleteCookies()
+                    .permitAll()
+                .and()
+                    .httpBasic()
+                .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
     }
@@ -58,5 +90,4 @@ public class SecurityConfig {
         roleHierarchy.setHierarchy(hierarchy);
         return roleHierarchy;
     }
-
 }
